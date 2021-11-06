ccaf3395e1cb637aed03c6bdc9ddfc24833f1d11
mohamadmek <45299765+mohamadmek@users.noreply.github.com>
2021-11-06 15:03:39 +0200

    Adding Models

---
+++ app/Models/Favorite.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'city', 'user_id'
+    ];
+}
